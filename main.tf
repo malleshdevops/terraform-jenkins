9daf3aea6226d9aeb9eb19f99fd50c1a84ead752
malleshdevops <81004732+malleshdevops@users.noreply.github.com>
2023-12-06 08:37:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.13.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.8.0"
+    }
+  }
+}
+
+variable "region" {
+  description = "The name of the aws Region"
+  type        = string
+}
+locals {
+  region = var.region
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "192.168.1.0/28"
+  name = "jenkins-test"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
